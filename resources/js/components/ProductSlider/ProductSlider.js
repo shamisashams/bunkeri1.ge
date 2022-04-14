30e52061066d2edcd08550e16a57c90e4e7b6c42
shamisashams <shamisaashams@gmail.com>
2022-04-14 12:51:54 +0400

    slider progress bar

--- resources/js/components/ProductSlider/ProductSlider.js
+++ resources/js/components/ProductSlider/ProductSlider.js
@@ -1,94 +1,123 @@
-import React from "react";
+import React, { useRef } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick/lib/slider";
 import { ProductBox } from "../ProductBox/ProductBox";
 import "./ProductSlider.css";
-import { NextSlide, PrevSlide, SliderButtons } from "../Buttons/Buttons";
+import { NextSlide, PrevSlide } from "../Buttons/Buttons";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination, Navigation } from "swiper";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+SwiperCore.use([Pagination, Navigation]);
 
 const ProductSlider = ({ data, head, rightBtns, showArrows }) => {
-  let settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    arrows: showArrows,
-    responsive: [
-      {
-        breakpoint: 1500,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 5,
-        },
-      },
-      {
-        breakpoint: 1300,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        },
-      },
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 760,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-        },
-      },
-    ],
-  };
+    const navigationPrevRef = useRef(null);
+    const navigationNextRef = useRef(null);
+    return (
+        <div className="product_slider">
+            <div className="flex head">
+                <div className="title35 blue">{head}</div>
+                {rightBtns ? (
+                    <div className="right_btns flex">
+                        {rightBtns.map((button, i) => {
+                            return <div key={i}>{button}</div>;
+                        })}
+                    </div>
+                ) : (
+                    ""
+                )}
+            </div>
+            <Swiper
+                modules={[Navigation, Pagination]}
+                navigation={{
+                    prevEl: navigationPrevRef.current,
+                    nextEl: navigationNextRef.current,
+                }}
+                onSwiper={(swiper) => {
+                    // Delay execution for the refs to be defined
+                    setTimeout(() => {
+                        // Override prevEl & nextEl now that refs are defined
+                        swiper.params.navigation.prevEl =
+                            navigationPrevRef.current;
+                        swiper.params.navigation.nextEl =
+                            navigationNextRef.current;
 
-  return (
-    <div className="product_slider">
-      <div className="flex head">
-        <div className="title35 blue">{head}</div>
-        {rightBtns ? (
-          <div className="right_btns flex">
-            {rightBtns.map((button, i) => {
-              return <div key={i}>{button}</div>;
-            })}
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-      <div className="progressbar">
-        <div className="fill"></div>
-      </div>
-      <Slider {...settings}>
-        {data.map((item, index) => {
-          return (
-            <ProductBox
-              key={index}
-              link={route('client.product.show',item.slug)}
-              img={( item.latest_image != null) ? '/' + item.latest_image.path + '/' + item.latest_image.title : null}
-              title={item.title}
-              price={item.special_price !== null ? item.special_price : item.price}
-              sale={item.special_price !== null ? true : false}
-              new={item.new}
-              product={item}
-            />
-          );
-        })}
-      </Slider>
-    </div>
-  );
+                        // Re-init navigation
+                        swiper.navigation.destroy();
+                        swiper.navigation.init();
+                        swiper.navigation.update();
+                    });
+                }}
+                pagination={{
+                    type: "progressbar",
+                }}
+                grabCursor
+                loop
+                slidesPerView={6}
+                // style={{ height: "220px" }}
+                breakpoints={{
+                    1500: {
+                        slidesPerView: 6,
+                    },
+                    1300: {
+                        slidesPerView: 5,
+                    },
+                    1000: {
+                        slidesPerView: 4,
+                    },
+                    600: {
+                        slidesPerView: 3,
+                    },
+                    450: {
+                        slidesPerView: 2,
+                    },
+                    200: {
+                        slidesPerView: 1,
+                    },
+                }}
+            >
+                {data.map((item, index) => {
+                    return (
+                        <SwiperSlide key={index}>
+                            <ProductBox
+                                link={route("client.product.show", item.slug)}
+                                img={
+                                    item.latest_image != null
+                                        ? "/" +
+                                          item.latest_image.path +
+                                          "/" +
+                                          item.latest_image.title
+                                        : null
+                                }
+                                title={item.title}
+                                price={
+                                    item.special_price !== null
+                                        ? item.special_price
+                                        : item.price
+                                }
+                                sale={
+                                    item.special_price !== null ? true : false
+                                }
+                                new={item.new}
+                                product={item}
+                            />
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+            <div className="flex centered slidebtns">
+                <div ref={navigationPrevRef}>
+                    <PrevSlide />
+                </div>
+                <div ref={navigationNextRef}>
+                    <NextSlide />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ProductSlider;
--- resources/js/pages/Home/Home.js
+++ resources/js/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {
-  CommonButton,
-  RoundButton,
-  SliderButtons,
+    CommonButton,
+    RoundButton,
+    SliderButtons,
 } from "../../components/Buttons/Buttons";
 import { ProductBox } from "../../components/ProductBox/ProductBox";
 import "./Home.css";
@@ -11,18 +11,21 @@ import { SliderData } from "../../components/ProductSlider/SliderData";
 import TodaysBox from "../../components/TodaysBox/TodaysBox";
 import Layout from "../../Layouts/Layout";
 import { usePage } from "@inertiajs/inertia-react";
-import {Inertia} from "@inertiajs/inertia";
+import { Inertia } from "@inertiajs/inertia";
 
-const renderHTML = (rawHTML) => React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
+const renderHTML = (rawHTML) =>
+    React.createElement("div", {
+        dangerouslySetInnerHTML: { __html: rawHTML },
+    });
 
-const Home = ({page,seo}) => {
+const Home = ({ page, seo }) => {
     const sharedData = usePage().props.localizations;
     const { images, products } = usePage().props;
-    console.log(products)
+    console.log(products);
 
-    const addToCart = function (product){
+    const addToCart = function (product) {
         //localStorage.removeItem('cart')
-        let _cart = localStorage.getItem('cart');
+        let _cart = localStorage.getItem("cart");
         let cart;
         if (_cart !== null) {
             cart = JSON.parse(_cart);
@@ -30,42 +33,37 @@ const Home = ({page,seo}) => {
 
         let qty = 1;
 
-
-        if(cart.length > 0){
+        if (cart.length > 0) {
             let exists = false;
-            cart.forEach(function (el,i){
-
-                if(el.product.id === product.id){
+            cart.forEach(function (el, i) {
+                if (el.product.id === product.id) {
                     el.qty += qty;
                     exists = true;
                 }
-
-            })
+            });
             if (!exists) {
                 let obj = {
                     product: product,
-                    qty: qty
-                }
+                    qty: qty,
+                };
                 cart.push(obj);
             }
-
         } else {
             let obj = {
                 product: product,
-                qty: qty
-            }
+                qty: qty,
+            };
             cart.push(obj);
         }
 
-
-        localStorage.setItem('cart',JSON.stringify(cart))
-        console.log(JSON.parse(localStorage.getItem('cart')))
+        localStorage.setItem("cart", JSON.stringify(cart));
+        console.log(JSON.parse(localStorage.getItem("cart")));
         //localStorage.removeItem('cart')
-        Inertia.visit(window.location.href)
-    }
+        Inertia.visit(window.location.href);
+    };
 
-    const buyNow = function (product){
-        let _cart = localStorage.getItem('cart');
+    const buyNow = function (product) {
+        let _cart = localStorage.getItem("cart");
         let cart;
         if (_cart !== null) {
             cart = JSON.parse(_cart);
@@ -73,104 +71,171 @@ const Home = ({page,seo}) => {
 
         let qty = 1;
 
-
-        if(cart.length > 0){
+        if (cart.length > 0) {
             let exists = false;
-            cart.forEach(function (el,i){
-
-                if(el.product.id === product.id){
+            cart.forEach(function (el, i) {
+                if (el.product.id === product.id) {
                     el.qty += qty;
                     exists = true;
                 }
-
-            })
+            });
             if (!exists) {
                 let obj = {
                     product: product,
-                    qty: qty
-                }
+                    qty: qty,
+                };
                 cart.push(obj);
             }
-
         } else {
             let obj = {
                 product: product,
-                qty: qty
-            }
+                qty: qty,
+            };
             cart.push(obj);
         }
 
-
-        localStorage.setItem('cart',JSON.stringify(cart))
-        console.log(JSON.parse(localStorage.getItem('cart')))
+        localStorage.setItem("cart", JSON.stringify(cart));
+        console.log(JSON.parse(localStorage.getItem("cart")));
         //localStorage.removeItem('cart')
-        Inertia.get(route('client.checkout.index'));
-    }
+        Inertia.get(route("client.checkout.index"));
+    };
 
-  return (
-      <Layout seo={seo}>
-          <div className="homePage">
-              <div className="hero_section" style={{ background: "url(" + images[0] + ")" }}>
-                  <div className="wrapper">
-                      <div className="title35 blue">{__('client.home_section1_title',sharedData)}</div>
-                      <div className="archy-edt blue">
-                          {renderHTML(__('client.home_section1_text',sharedData).newLineToBr())}
-                      </div>
-                      <RoundButton link="/" text={__('client.home_section1_btn_title',sharedData)} />
-                  </div>
-              </div>
-              <div className="new_products wrapper">
-                  <ProductSlider
-                      showArrows
-                      head={__('client.home_new_collection',sharedData)}
-                      data={products.new_collection}
-                      rightBtns={[
-                          <div>
-                              <CommonButton link="/#special_price" text={__('client.home_slider_special_price',sharedData)} />
-                              <CommonButton gray link="/" text={__('client.home_slider_new',sharedData)} />
-                              <CommonButton gray link="/" text={__('client.home_slider_popular',sharedData)} />
-                          </div>,
-                          <SliderButtons />,
-                      ]}
-                  />
-              </div>
-              <div className="bunker">
-                  <div className="wrapper">
-                      <ProductSlider head={__('client.home_bunker',sharedData)} data={products.bunker} />
-                  </div>
-              </div>
-              <div className="today wrapper">
-                  <TodaysBox addTocart={addToCart} buyNow={buyNow} day_product={products.day_product} day_price={products.day_price} />
-              </div>
-              <div className="special_price" id="special_price">
-                  <div className="wrapper">
-                      <ProductSlider
-                          head={__('client.home_special_price',sharedData)}
-                          data={products.special_price_tag}
-                          rightBtns={[<CommonButton link={route('client.category.special')} text={__('client.home_btn_view_all',sharedData)} />]}
-                      />
-                  </div>
-              </div>
-              <div id="popular_products" className="popular wrapper">
-                  <ProductSlider
-                      head={__('client.home_popular',sharedData)}
-                      data={products.popular}
-                      rightBtns={[<CommonButton link={route('client.category.popular')} text={__('client.home_btn_view_all',sharedData)} />]}
-                  />
-              </div>
-              <div className="new_collection">
-                  <div className="wrapper">
-                      <div className="title35">{__('client.home_section2_title',sharedData)}</div>
-                      <div className="archy-edt">
-                          {renderHTML(__('client.home_section2_text',sharedData).newLineToBr())}
-                      </div>
-                      <RoundButton white text={__('client.home_section2_btn_txt',sharedData)} link="/" />
-                  </div>
-              </div>
-          </div>
-
-      </Layout>
-  );
+    return (
+        <Layout seo={seo}>
+            <div className="homePage">
+                <div
+                    className="hero_section"
+                    style={{ background: "url(" + images[0] + ")" }}
+                >
+                    <div className="wrapper">
+                        <div className="title35 blue">
+                            {__("client.home_section1_title", sharedData)}
+                        </div>
+                        <div className="archy-edt blue">
+                            {renderHTML(
+                                __(
+                                    "client.home_section1_text",
+                                    sharedData
+                                ).newLineToBr()
+                            )}
+                        </div>
+                        <RoundButton
+                            link="/"
+                            text={__(
+                                "client.home_section1_btn_title",
+                                sharedData
+                            )}
+                        />
+                    </div>
+                </div>
+                <div className="new_products wrapper">
+                    <ProductSlider
+                        showArrows
+                        head={__("client.home_new_collection", sharedData)}
+                        data={products.new_collection}
+                        rightBtns={[
+                            <div>
+                                <CommonButton
+                                    link="/#special_price"
+                                    text={__(
+                                        "client.home_slider_special_price",
+                                        sharedData
+                                    )}
+                                />
+                                <CommonButton
+                                    gray
+                                    link="/"
+                                    text={__(
+                                        "client.home_slider_new",
+                                        sharedData
+                                    )}
+                                />
+                                <CommonButton
+                                    gray
+                                    link="/"
+                                    text={__(
+                                        "client.home_slider_popular",
+                                        sharedData
+                                    )}
+                                />
+                            </div>,
+                        ]}
+                    />
+                </div>
+                <div className="bunker">
+                    <div className="wrapper">
+                        <ProductSlider
+                            head={__("client.home_bunker", sharedData)}
+                            data={products.bunker}
+                        />
+                    </div>
+                </div>
+                <div className="today wrapper">
+                    <TodaysBox
+                        addTocart={addToCart}
+                        buyNow={buyNow}
+                        day_product={products.day_product}
+                        day_price={products.day_price}
+                    />
+                </div>
+                <div className="special_price" id="special_price">
+                    <div className="wrapper">
+                        <ProductSlider
+                            head={__("client.home_special_price", sharedData)}
+                            data={products.special_price_tag}
+                            rightBtns={[
+                                <CommonButton
+                                    link={route("client.category.special")}
+                                    text={__(
+                                        "client.home_btn_view_all",
+                                        sharedData
+                                    )}
+                                />,
+                            ]}
+                        />
+                    </div>
+                </div>
+                <div id="popular_products" className="popular wrapper">
+                    <ProductSlider
+                        head={__("client.home_popular", sharedData)}
+                        data={products.popular}
+                        rightBtns={[
+                            <CommonButton
+                                link={route("client.category.popular")}
+                                text={__(
+                                    "client.home_btn_view_all",
+                                    sharedData
+                                )}
+                            />,
+                        ]}
+                    />
+                </div>
+                <div className="new_collection">
+                    <div className="wrapper">
+                        <div className="title35">
+                            {__("client.home_section2_title", sharedData)}
+                        </div>
+                        <div className="archy-edt">
+                            {renderHTML(
+                                __(
+                                    "client.home_section2_text",
+                                    sharedData
+                                ).newLineToBr()
+                            )}
+                        </div>
+                        <RoundButton
+                            white
+                            text={__(
+                                "client.home_section2_btn_txt",
+                                sharedData
+                            )}
+                            link="/"
+                        />
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default Home;

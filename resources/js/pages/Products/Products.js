b782c1bfaa0d0d414aa9e583705b7330a2915cba
shamisashams <shamisaashams@gmail.com>
2022-04-14 11:15:17 +0400

    select drop

--- resources/js/pages/Products/Products.js
+++ resources/js/pages/Products/Products.js
@@ -1,61 +1,67 @@
 import React, { useState } from "react";
 import Filters from "../../components/Filters/Filters";
-import Select from "react-select";
 import makeAnimated from "react-select/animated";
 import "./Products.css";
-import { productsData } from "./ProductsData";
 import { ProductBox } from "../../components/ProductBox/ProductBox";
 import { Arrow } from "../../components/SmallComps/Icons";
 import Layout from "../../Layouts/Layout";
 import { usePage } from "@inertiajs/inertia-react";
 import { Link } from "@inertiajs/inertia-react";
-import {Inertia} from "@inertiajs/inertia";
+import { Inertia } from "@inertiajs/inertia";
 
-
-let links = function (links){
+let links = function (links) {
     let rows = [];
     //links.shift();
     //links.splice(-1);
-    {links.map(function (item,index){
+    {
+        links.map(function (item, index) {
+            if (index > 0 && index < links.length - 1) {
+                rows.push(
+                    <Link
+                        href={item.url}
+                        className={item.active ? "num active" : "num"}
+                    >
+                        {item.label}
+                    </Link>
+                );
+            }
+        });
+    }
+    return <div className="nums"> {rows.length > 1 ? rows : null} </div>;
+};
 
-
-        if (index > 0 && index < links.length - 1){
-            rows.push(<Link href={item.url} className={(item.active) ? 'num active': 'num'}>{item.label}</Link>)
-        }
-
-    })}
-    return <div className="nums"> {rows.length > 1 ? rows : null} </div>
-}
-
-let linksPrev = function (links){
+let linksPrev = function (links) {
     let rowCount = 0;
-    links.map(function (item,index){
-        if (index > 0 && index < links.length - 1){
+    links.map(function (item, index) {
+        if (index > 0 && index < links.length - 1) {
             rowCount++;
         }
-    })
-    return rowCount > 1 ? <Link href={links[0].url}>
-        <Arrow color="#2F3E51" rotate="90" />
-        <Arrow color="#2F3E51" rotate="90" />
-    </Link> : null
-}
-let linksNext = function (links){
+    });
+    return rowCount > 1 ? (
+        <Link href={links[0].url}>
+            <Arrow color="#2F3E51" rotate="90" />
+            <Arrow color="#2F3E51" rotate="90" />
+        </Link>
+    ) : null;
+};
+let linksNext = function (links) {
     let rowCount = 0;
-    links.map(function (item,index){
-        if (index > 0 && index < links.length - 1){
+    links.map(function (item, index) {
+        if (index > 0 && index < links.length - 1) {
             rowCount++;
         }
-    })
-    return rowCount > 1 ? <Link href={links[links.length - 1].url}>
-        <Arrow color="#2F3E51" rotate="-90" />
-        <Arrow color="#2F3E51" rotate="-90" />
-    </Link> : null
-}
+    });
+    return rowCount > 1 ? (
+        <Link href={links[links.length - 1].url}>
+            <Arrow color="#2F3E51" rotate="-90" />
+            <Arrow color="#2F3E51" rotate="-90" />
+        </Link>
+    ) : null;
+};
 
-const addToCart = function (product){
-
+const addToCart = function (product) {
     //localStorage.removeItem('cart')
-    let _cart = localStorage.getItem('cart');
+    let _cart = localStorage.getItem("cart");
     let cart;
     if (_cart !== null) {
         cart = JSON.parse(_cart);
@@ -63,165 +69,210 @@ const addToCart = function (product){
 
     let qty = 1;
 
-
-    if(cart.length > 0){
+    if (cart.length > 0) {
         let exists = false;
-        cart.forEach(function (el,i){
-
-            if(el.product.id === product.id){
+        cart.forEach(function (el, i) {
+            if (el.product.id === product.id) {
                 el.qty += qty;
                 exists = true;
             }
-
-        })
+        });
         if (!exists) {
             let obj = {
                 product: product,
-                qty: qty
-            }
+                qty: qty,
+            };
             cart.push(obj);
         }
-
     } else {
         let obj = {
             product: product,
-            qty: qty
-        }
+            qty: qty,
+        };
         cart.push(obj);
     }
 
-
-    localStorage.setItem('cart',JSON.stringify(cart))
-    console.log(JSON.parse(localStorage.getItem('cart')))
+    localStorage.setItem("cart", JSON.stringify(cart));
+    console.log(JSON.parse(localStorage.getItem("cart")));
     //localStorage.removeItem('cart')
-}
+};
 
-const Products = ({page,seo}) => {
+const Products = ({ page, seo }) => {
     const sharedData = usePage().props.localizations;
-  const [showFilter, setShowFilter] = useState(false);
-  const toggleFilter = () => {
-    setShowFilter(!showFilter);
-  };
-  const animatedComponents = makeAnimated();
-  const options = [
-    { value: "price", label: "ფასის ზრდადობით" },
-    { value: "ფასის კლებით", label: "ფასის კლებით" },
-    { value: "თარიღის მიხედვით", label: "თარიღის მიხედვით" },
-  ];
+    const [showFilter, setShowFilter] = useState(false);
+    const toggleFilter = () => {
+        setShowFilter(!showFilter);
+    };
+    const animatedComponents = makeAnimated();
 
     const { products, category, images } = usePage().props;
 
     console.log(products);
     console.log(category);
 
-
-
-    const sort = function (data){
+    const sort = function (data) {
         let appliedFilters = [];
         let urlParams = new URLSearchParams(window.location.search);
 
         urlParams.forEach((value, index) => {
-            appliedFilters[index] = value.split(',');
+            appliedFilters[index] = value.split(",");
         });
-        console.log(data)
+        console.log(data);
 
         urlParams.forEach((value, index) => {
-            appliedFilters[index] = value.split(',');
+            appliedFilters[index] = value.split(",");
         });
 
-        appliedFilters['sort'] = data.sort;
-        appliedFilters['order'] = data.order;
+        appliedFilters["sort"] = data.sort;
+        appliedFilters["order"] = data.order;
 
         console.log(appliedFilters);
         let params = [];
 
-        for(let key in appliedFilters) {
-            if(Array.isArray(appliedFilters[key])){
-                params.push(key + '=' + appliedFilters[key].join(','))
+        for (let key in appliedFilters) {
+            if (Array.isArray(appliedFilters[key])) {
+                params.push(key + "=" + appliedFilters[key].join(","));
             } else {
-                params.push(key + '=' + appliedFilters[key])
+                params.push(key + "=" + appliedFilters[key]);
             }
-
         }
 
-        Inertia.visit("?" + params.join('&'));
-    }
+        Inertia.visit("?" + params.join("&"));
+    };
+    const selectOptions = [
+        {
+            label: "newest",
+            sort: "created_at",
+            order: "desc",
+        },
+        {
+            label: "oldest",
+            sort: "created_at",
+            order: "asc",
+        },
+        {
+            label: "cheaper",
+            sort: "price",
+            order: "asc",
+        },
+        {
+            label: "expensive",
+            sort: "price",
+            order: "asc",
+        },
+        {
+            label: "a-z",
+            sort: "title",
+            order: "asc",
+        },
+        {
+            label: "z-a",
+            sort: "title",
+            order: "asc",
+        },
+    ];
+    const [selected, setSelected] = useState(0);
+    const handleOption = (index) => {
+        sort({
+            sort: selectOptions[index].sort,
+            order: selectOptions[index].order,
+        });
+        setSelected(index);
+    };
 
-  return (
-      <Layout seo={seo}>
-        <div className="productsPage">
-          <div className="wrapper flex main">
-            <div
-              className={
-                showFilter ? "column filter_column show" : "column filter_column"
-              }
-            >
-              <div className="headtitle">{__('client.products_filter_title',sharedData)}</div>
+    return (
+        <Layout seo={seo}>
+            <div className="productsPage">
+                <div className="wrapper flex main">
+                    <div
+                        className={
+                            showFilter
+                                ? "column filter_column show"
+                                : "column filter_column"
+                        }
+                    >
+                        <div className="headtitle">
+                            {__("client.products_filter_title", sharedData)}
+                        </div>
 
-              <Filters />
-            </div>
-            <div className="column pro_co">
-              <button className="filter_btn" onClick={() => toggleFilter()}>
-                <img src="/img/icons/other/filter.png" alt="" />
-              </button>
-              <div className="flex headflex">
-                <div className="headtitle">{category !== null ? category.title : null}</div>
-                <Select
-                  closeMenuOnSelect={true}
-                  components={animatedComponents}
-                  placeholder="თარიღის მიხედვით"
-                  isMulti={false}
-                  options={options}
-                />
-                  <button onClick={() => sort({sort:"created_at",order:"desc"})}>newest</button>
-                  <button onClick={() => sort({sort:"created_at",order:"asc"})}>oldest</button>
-                  <button onClick={() => sort({sort:"price",order:"asc"})}>cheaper</button>
-                  <button onClick={() => sort({sort:"price",order:"desc"})}>expensive</button>
-                  <button onClick={() => sort({sort:"title",order:"asc"})}>a-z</button>
-                  <button onClick={() => sort({sort:"title",order:"desc"})}>z-a</button>
-              </div>
-              <div className="product_grid">
-                {products.data.map((data, index) => {
-                  return (
-                    <ProductBox
-                      key={index}
-                      link={route('client.product.show',data.slug)}
-                      img={( data.latest_image != null) ? '/' + data.latest_image.path + '/' + data.latest_image.title : null}
-                      title={data.title}
-                      price={data.special_price !== null ? data.special_price : data.price}
-                      sale={data.special_price !== null ? true : false}
-                      new={data.new}
-                      product={data}
-                      handleClick={() => addToCart(data)}
-                    />
-                  );
-                })}
-              </div>
-              <div className="pagination flex centered">
-                {/*<button>
-                  <Arrow color="#2F3E51" rotate="90" />
-                  <Arrow color="#2F3E51" rotate="90" />
-                </button>
-                <div className="nums">
-                  <button className="num">1</button>
-                  <button className="num">2</button>
-                  <button className="num">3</button>
-                  <button className="num">4</button>
+                        <Filters />
+                    </div>
+                    <div className="column pro_co">
+                        <button
+                            className="filter_btn"
+                            onClick={() => toggleFilter()}
+                        >
+                            <img src="/img/icons/other/filter.png" alt="" />
+                        </button>
+                        <div className="flex headflex">
+                            <div className="headtitle">
+                                {category !== null ? category.title : null}
+                            </div>
+                            <div className="select_dropdown">
+                                <div className="selected">
+                                    {selectOptions[selected].label}
+                                </div>
+                                <div className="options">
+                                    {selectOptions.map((opt, index) => {
+                                        return (
+                                            <button
+                                                key={index}
+                                                onClick={() =>
+                                                    handleOption(index)
+                                                }
+                                            >
+                                                {opt.label}
+                                            </button>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product_grid">
+                            {products.data.map((data, index) => {
+                                return (
+                                    <ProductBox
+                                        key={index}
+                                        link={route(
+                                            "client.product.show",
+                                            data.slug
+                                        )}
+                                        img={
+                                            data.latest_image != null
+                                                ? "/" +
+                                                  data.latest_image.path +
+                                                  "/" +
+                                                  data.latest_image.title
+                                                : null
+                                        }
+                                        title={data.title}
+                                        price={
+                                            data.special_price !== null
+                                                ? data.special_price
+                                                : data.price
+                                        }
+                                        sale={
+                                            data.special_price !== null
+                                                ? true
+                                                : false
+                                        }
+                                        new={data.new}
+                                        product={data}
+                                        handleClick={() => addToCart(data)}
+                                    />
+                                );
+                            })}
+                        </div>
+                        <div className="pagination flex centered">
+                            {linksPrev(products.links)}
+                            {links(products.links)}
+                            {linksNext(products.links)}
+                        </div>
+                    </div>
                 </div>
-
-                <button>
-                  <Arrow color="#2F3E51" rotate="-90" />
-                  <Arrow color="#2F3E51" rotate="-90" />
-                </button>*/}
-                  {linksPrev(products.links)}
-                  {links(products.links)}
-                  {linksNext(products.links)}
-              </div>
             </div>
-          </div>
-        </div>
-      </Layout>
-  );
+        </Layout>
+    );
 };
 
 export default Products;
